d80456dcab4c8080c8379801fb9bed6f18a17819
Hannes Sörensson <hannes.sorensson@gmail.com>
2022-01-28 19:21:30 +0100

    Angry eyebrows, and .gitignore for TIC-generated dir

--- duo.lua
+++ duo.lua
@@ -21,10 +21,10 @@ function TIC()
 end
 
 -- <TILES>
--- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
--- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
--- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
--- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 001:eccccccccc888888caaaaaaaca888888cac0cccccacc00c0cacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceec0ca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccac0cccccacc00c0cacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccc0ca0c0c0cca0c0c0cca0c0c
 -- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
 -- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
 -- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
